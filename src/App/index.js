eab45e328b912cab857862b62c821822f47ec575
avatarmario <tapiamariomx@gmail.com>
2026-07-09 22:33:28 -0700

    class22-Done-2nd-commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,88 +1,13 @@
 import React from 'react';
-
-import { useLocalStorage } from './useLocalStorage';
+import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-/* const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true},
-  { text: 'Tomar el Curso de Intro a React.js', completed: false},
-  { text: 'Llorar con la llorona', completed: false},
-  { text: 'LALALALALA', completed: false},
-  { text: 'Usar estados derivados', completed: true},
-
-]
-
-localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-localStorage.removeItem('TODOS_V1'); */
-
-
 function App(){
-  // ESTADOS
-
-  // State for: todos
-  const {
-    item: todos,
-    saveItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage('TODOS_V1', []);
-
-  // State for: search input
-  const [searchValue, setSearchValue] = React.useState('');
-
-  // ESTADOS DERIVADOS 
-
-  // Count: todos completed
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
-  // Count: total todos
-  const totalTodos = todos.length;
-
-  
-
-  // clase 9: filter todos
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLocaleLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
-
-  // clase 10 completar todos con eventos 
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  // clase 10 ¡ eliminar todos con eventos 
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
 
   return (
-    <AppUI
-      loading = {loading}
-      error = {error}
-      completedTodos = {completedTodos}
-      totalTodos = {totalTodos}
-      searchValue = {searchValue}
-      setSearchValue = {setSearchValue}
-      searchedTodos = {searchedTodos}
-      completeTodo = {completeTodo}
-      deleteTodo = {deleteTodo}
-
-    />
+    <TodoProvider>
+      <AppUI/>
+    </TodoProvider>
   );
   
 }
---
+++ src/TodoContext/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useLocalStorage } from './useLocalStorage';
+const TodoContext = React.createContext();
+
+function TodoProvider({children}) {
+    // ESTADOS
+
+    // State for: todos
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage('TODOS_V1', []);
+
+    // State for: search input
+    const [searchValue, setSearchValue] = React.useState('');
+
+    // ESTADOS DERIVADOS 
+
+    // Count: todos completed
+    const completedTodos = todos.filter(
+        todo => !!todo.completed
+    ).length;
+    // Count: total todos
+    const totalTodos = todos.length;
+
+    
+
+    // clase 9: filter todos
+    const searchedTodos = todos.filter(
+        (todo) => {
+        const todoText = todo.text.toLowerCase();
+        const searchText = searchValue.toLocaleLowerCase();
+        return todoText.includes(searchText);
+        }
+    );
+
+    // clase 10 completar todos con eventos 
+    const completeTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex(
+        (todo) => todo.text === text
+        );
+        newTodos[todoIndex].completed = true;
+        saveTodos(newTodos);
+    };
+
+    // clase 10 ¡ eliminar todos con eventos 
+    const deleteTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex(
+        (todo) => todo.text === text
+        );
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+    };
+
+    return(
+        <TodoContext.Provider value = {{
+            loading,
+            error,
+            completedTodos,
+            totalTodos,
+            searchValue,
+            setSearchValue,
+            searchedTodos,
+            completeTodo,
+            deleteTodo, 
+        }}>
+            {children}
+        </TodoContext.Provider>
+
+    );
+}
+
+export { TodoContext, TodoProvider }
